db746ccbab7c67b1e20b5dfac6015bb5484ee09e
Robin Deits <robin.deits@gmail.com>
2016-12-02 01:11:37 -0500

    add dictionaries

--- src/wordsets.jl
+++ src/wordsets.jl
@@ -4,18 +4,22 @@ typealias Wordset Set{String}
 
 cleanup_phrase(phrase) = replace(lowercase(phrase), r"[^a-z ]", "")
 
+read_dictionary(file) = Set(cleanup_phrase.(readdlm(file, '\t', String)))
+
+UKACD() = read_dictionary("data/UKACD.txt")
+sowpods() = read_dictionary("data/sowpods.txt")
+words() = Set(cleanup_phrase.(readdlm("/usr/share/dict/words", '\t', String, use_mmap=false)))
+
 module Wikipedia
+    import Wordsets: Wordset, cleanup_phrase
+    using Wiki
 
-import Wordsets: Wordset, cleanup_phrase
-using Wiki
+    cleanup_wiki_title(title) = replace(title, r"\([^\)]*\)", "")
 
-cleanup_wiki_title(title) = replace(title, r"\([^\)]*\)", "")
-
-function wordset(title)
-    titles = links(WikiPage(title))
-    Wordset(cleanup_phrase.(cleanup_wiki_title.(titles)))
-end
-
+    function wordset(title)
+        titles = links(WikiPage(title))
+        Wordset(cleanup_phrase.(cleanup_wiki_title.(titles)))
+    end
 end
 
 end
