def364ac11b4b38d3ac317fd45e774a0b0741572
Robin Deits <robin.deits@gmail.com>
2017-01-28 23:07:55 -0500

    fix data paths

--- src/wordsets.jl
+++ src/wordsets.jl
@@ -6,8 +6,8 @@ cleanup_phrase(phrase) = replace(lowercase(phrase), r"[^a-z ]", "")
 
 read_dictionary(file) = cleanup_phrase.(vec(readdlm(file, '\t', String)))
 
-UKACD() = read_dictionary("data/UKACD.txt")
-sowpods() = read_dictionary("data/sowpods.txt")
+UKACD() = read_dictionary(joinpath(dirname(@__FILE__), "..", "data", "UKACD.txt"))
+sowpods() = read_dictionary(joinpath(dirname(@__FILE__), "..", "data", "sowpods.txt"))
 unixwords() = cleanup_phrase.(vec(readdlm("/usr/share/dict/words", '\t', String, use_mmap=false)))
 
 module Wikipedia
