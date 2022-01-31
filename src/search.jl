e9a6889688fe3c5929cd99ad5d9db2a5b61488a6
Robin Deits <robin.deits@gmail.com>
2022-01-30 21:27:26 -0500

    add DFS over arbitrary graphs and allow :good_and_partial results

--- src/search.jl
+++ src/search.jl
@@ -1,5 +1,7 @@
 module Search
 
+using Graphs: AbstractGraph, outneighbors, SimpleGraph, add_edge!
+
 export dfs
 
 struct LazyDFS{T, FC <: Function, FE <: Function}
@@ -27,52 +29,68 @@ function Base.iterate(dfs::LazyDFS)
     end
     _, state = itr
     iterate(dfs,
-        ([dfs.start], [(PausedIterator{typeof(neighbors), typeof(state)}(), false)]))
+        ([dfs.start], [PausedIterator{typeof(neighbors), typeof(state)}()]))
 end
 
-function Base.iterate(dfs::LazyDFS, (path, metadata))
+function maybe_push!(x::AbstractVector{T}, y::T) where {T}
+    push!(x, y)
+    x
+end
+
+function maybe_push!(x::AbstractVector{T1}, y::T2) where {T1, T2}
+    dest = similar(x, Base.promote_typejoin(T1, T2), length(x) + 1)
+    copyto!(dest, x)
+    dest[end] = y
+    dest
+end
+
+function backtrack!(path, metadata)
     while !isempty(path)
-        meta, explored = pop!(metadata)
-        if !explored
-            result = dfs.evaluate(path)
-            if result == :good
-                push!(metadata, (meta, true))
-                return copy(path), (path, metadata)
-            elseif result == :partial
-                child_iterator = dfs.children(path)
-                if child_iterator === nothing
-                    pop!(path)
-                else
-                    itr = iterate(child_iterator)
-                    if itr !== nothing
-                        child, child_state = itr
-                        push!(metadata, (meta, true))
-                        push!(path, child)
-                        new_metadata = (PausedIterator(child_iterator, child_state), false)
-                        if typeof(new_metadata) <: eltype(metadata)
-                            push!(metadata, new_metadata)
-                        else
-                            metadata = vcat(metadata, new_metadata)
-                        end
-                        continue
-                    else
-                        pop!(path)
-                    end
-                end
-            else
-                pop!(path)
-            end
-        else
-            pop!(path)
-        end
-        @assert length(path) == length(metadata)
+        pop!(path)
+        meta = pop!(metadata)
         if !isempty(path)
             itr = iterate(meta.iterator, meta.state)
             if itr !== nothing
                 node, state = itr
                 push!(path, node)
-                push!(metadata, (PausedIterator(meta.iterator, state), false))
+                metadata = maybe_push!(metadata, PausedIterator(meta.iterator, state))
+                return path, metadata
             end
+        end
+    end
+    return path, metadata
+end
+
+function next_node(dfs::LazyDFS, (path, metadata))
+    # First, see if we can explore deeper
+    child_iterator = dfs.children(path)
+    if child_iterator !== nothing
+        itr = iterate(child_iterator)
+        if itr !== nothing
+            child, child_state = itr
+            push!(path, child)
+            metadata = maybe_push!(metadata, PausedIterator(child_iterator, child_state))
+            return (path, metadata)
+        end
+    end
+    return backtrack!(path, metadata)
+end
+
+function Base.iterate(dfs::LazyDFS, (path, metadata))
+    while !isempty(path)
+        result = dfs.evaluate(path)
+        if result == :good
+            output = copy(path)
+            path, metadata = backtrack!(path, metadata)
+            return output, (path, metadata)
+        elseif result == :good_and_partial
+            output = copy(path)
+            path, metadata = next_node(dfs, (path, metadata))
+            return output, (path, metadata)
+        elseif result == :bad
+            path, metadata = backtrack!(path, metadata)
+        elseif result == :partial
+            path, metadata = next_node(dfs, (path, metadata))
         end
     end
     return nothing
@@ -88,9 +106,12 @@ Depth-first search starting at `start`. Exploration is controlled by two functio
 
     evaluate(path) -> Return a Symbol describing the given path in the search. Returns one of:
 
-    * :good -> This path is done (i.e. has reached the goal set)
+    * :good -> This path is done (i.e. has reached the goal set and should not be explored further)
+    * :good_and_partial -> This path has reached the goal set, but should be explored further too.
     * :bad -> This path is impossible, and no children should be explored
     * :partial -> This path is not known to be bad, so we should explore its children
+
+The results of the search will be the set of paths which were rated as `:good` or `:good_and_partial`.
 
 Returns an iterator which performs the search lazily on demand. To get a single result, you can do:
 
@@ -108,6 +129,36 @@ Or to get all results in a vector, you can do:
 """
 function dfs(start::S, children::FC, evaluate::FE) where {S, FC, FE}
     return LazyDFS{S, FC, FE}(start, children, evaluate)
+end
+
+"""
+Depth-first search over the given graph.
+"""
+function Search.dfs(graph::AbstractGraph, start, evaluate; allow_cycles=true)
+    if allow_cycles
+        children = path -> outneighbors(graph, last(path))
+    else
+        children = path -> (i for i in outneighbors(graph, last(path))
+            if i ∉ path)
+    end
+    dfs(start, children, evaluate)
+end
+
+
+"""
+Create a graph corresponding to a rectangular grid with the given dimensions, including diagonal connections. See `Graphs.grid` from `Graphs.jl` for the non-diagonal variant.
+"""
+function grid_with_diagonals(dims::NTuple{N}) where {N}
+    graph = SimpleGraph(prod(dims))
+    L = LinearIndices(dims)
+    for I in CartesianIndices(dims)
+        indices = Tuple(I)
+        ranges = UnitRange.(max.(1, indices .- 1), min.(indices .+ 1, dims))
+        for other in Iterators.product(ranges...)
+            add_edge!(graph, L[I], L[other...])
+        end
+    end
+    graph
 end
 
 end
--- test/search.jl
+++ test/search.jl
@@ -1,5 +1,8 @@
 using Test
-using PuzzleTools.Search
+using PuzzleTools.Search: dfs
+using PuzzleTools.Words
+using Graphs: SimpleDiGraph, add_edge!
+using PuzzleTools: is_valid_prefix
 
 @testset "graph_search" begin
     actions = Dict(
@@ -29,13 +32,35 @@ using PuzzleTools.Search
         24 => []
     )
 
-    transitions(x) = (y for y in actions[last(x)] if y ∉ x)
-    function evaluate(x)
-        if length(x) == 24
-            :good
-        else
-            :partial
+    graph = SimpleDiGraph(24)
+    for (a, dests) in actions
+        for b in dests
+            add_edge!(graph, a, b)
         end
     end
-    @test only(dfs(1, transitions, evaluate)) == [1, 11, 4, 3, 8, 6, 17, 19, 15, 12, 23, 10, 16, 5, 2, 9, 18, 20, 21, 22, 13, 14, 7, 24]
+    evaluate = path -> length(path) == 24 ? :good : :partial
+    @test only(dfs(graph, 1, evaluate; allow_cycles=false)) == [1, 11, 4, 3, 8, 6, 17, 19, 15, 12, 23, 10, 16, 5, 2, 9, 18, 20, 21, 22, 13, 14, 7, 24]
+end
+
+@testset "dictionary search" begin
+    # Run a depth-first search over strings, and make sure the result is exactly the same as the corpus itself
+
+    text = """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"""
+    corpus = sort(unique(split(replace(lowercase(text), r"[^a-z]" => ""))))
+
+    children(path) = 'a':'z'
+
+    function evaluate(path)
+        if join(@view path[2:end]) in corpus
+            return :good_and_partial
+        elseif is_valid_prefix(corpus, join(@view path[2:end]))
+            return :partial
+        else
+            return :bad
+        end
+    end
+
+    search = dfs(Char(0), children, evaluate)
+    @test [join(w[2:end]) for w in search] == corpus
 end
