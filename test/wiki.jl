63ccce841b49b7c9d5f512d79fa87113c949c07c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-27 12:59:23 +0800

    Delete test directory

--- test/wiki.jl
+++
@@ -1,8 +0,0 @@
-using Test
-using PuzzleTools.Wiki
-
-@testset "Wiki Queries" begin
-    results = Wiki.search("fish")
-    @test "Salmon" in title.(links(first(results)))
-    @test "goose" in Wiki.corpus("bird")
-end
--- test/words.jl
+++
@@ -1,7 +0,0 @@
-using Test
-using PuzzleTools.Words: sowpods, UKACD
-
-@testset "Words" begin
-    @test "aaronsbeard" in UKACD()
-    @test "aaronsbeard" ∉ sowpods()
-end
